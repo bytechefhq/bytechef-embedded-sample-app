88a269efd5b5b9caee2c8a7c83ec9019d34dfe5e
Ivica Cardic <ivica.cardic@gmail.com>
2026-06-05 07:54:44 +0200

    520 EmbeddedWorkflowBuilder includeComponents optional (omit instead of empty array)

--- front-end/src/app/automations/[workflowUuid]/page.tsx
+++ front-end/src/app/automations/[workflowUuid]/page.tsx
@@ -10,7 +10,6 @@ export default async function AutomationPage({params}: {params: {workflowUuid: s
     baseUrl={`${process.env.BYTECHEF_APP_BASE_URL??'http://127.0.0.1:5173'}`}
     connectionDialogAllowed={true}
     environment={'DEVELOPMENT'}
-    includeComponents={[]}
     jwtToken={jwtToken}
     sharedConnectionIds={[1072]}
     workflowUuid={workflowUuid} />;
